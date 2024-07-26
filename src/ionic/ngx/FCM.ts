582e970c746ced5a4cab714b81612ebf49f23a4d
Digvijaysinh Gohil <digvijay@logisticinfotech.co.in>
2024-07-26 14:14:23 +0530

    Error fixed.

--- src/ionic/ngx/FCM.ts
+++ src/ionic/ngx/FCM.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { Observable, Subject } from 'rxjs'
-import { IonicNativePlugin } from '@ionic-native/core'
+import { AwesomeCordovaNativePlugin } from '@awesome-cordova-plugins/core'
 import type { FCMPlugin } from '../../www/FCMPlugin'
 import type { IChannelConfiguration } from '../../www/IChannelConfiguration'
 import type { INotificationPayload } from '../../www/INotificationPayload'
@@ -19,12 +19,12 @@ export class FCM {
     public static repo: string =
         'https://github.com/andrehtissot/cordova-plugin-fcm-with-dependecy-updated'
     public static platforms: string[] = ['Android', 'iOS']
-    public static installed: () => boolean = IonicNativePlugin.installed
-    public static getPlugin: () => any = IonicNativePlugin.getPlugin
-    public static getPluginName: () => string = IonicNativePlugin.getPluginName
-    public static getPluginRef: () => string = IonicNativePlugin.getPluginRef
-    public static getPluginInstallName: () => string = IonicNativePlugin.getPluginInstallName
-    public static getSupportedPlatforms: () => string[] = IonicNativePlugin.getSupportedPlatforms
+    public static installed: () => boolean = AwesomeCordovaNativePlugin.installed
+    public static getPlugin: () => any = AwesomeCordovaNativePlugin.getPlugin
+    public static getPluginName: () => string = AwesomeCordovaNativePlugin.getPluginName
+    public static getPluginRef: () => string = AwesomeCordovaNativePlugin.getPluginRef
+    public static getPluginInstallName: () => string = AwesomeCordovaNativePlugin.getPluginInstallName
+    public static getSupportedPlatforms: () => string[] = AwesomeCordovaNativePlugin.getSupportedPlatforms
 
     /** @copyFrom typings/FCMPlugin.d.ts FCMPlugin clearAllNotifications */
     public clearAllNotifications(): Promise<void> {
